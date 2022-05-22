5a09f1bd5cc8dad0230617cee6f2b587e795066f
kamiloudis <105654872+kamiloudis@users.noreply.github.com>
2022-05-22 16:07:32 +0300

    3.8 Query

---
+++ 3.8Query.sql
@@ -0,0 +1,21 @@
+-- Query 3.8 (OF COURSE researchers work only on active projects, who would have thought that)
+SELECT r.researcher_id, r.first_name, r.last_name, count(*) projects_without_deliverable
+FROM researcher r
+INNER JOIN works_on_project w
+ON w.researcher_id = r.researcher_id
+INNER JOIN
+(SELECT p.project_id
+FROM project p
+WHERE p.project_id NOT IN
+(
+	SELECT p.project_id
+	FROM project p
+	INNER JOIN deliverable d
+	ON d.project_id = p.project_id
+)
+AND (datediff(p.start_date, current_date()) <= 0 AND datediff(current_date(), p.end_date) <= 0)
+ORDER BY p.project_id
+) nodel_projects
+ON nodel_projects.project_id = w.project_id
+GROUP BY r.first_name, r.last_name, r.researcher_id
+HAVING count(*) >= 1;
