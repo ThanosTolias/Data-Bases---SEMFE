0b5399ba1bb99c9f0941d82803ff67c71b8c3276
ThanosTolias <90418936+ThanosTolias@users.noreply.github.com>
2022-05-22 16:28:36 +0300

    Query 3.3 working - without adjastments for input

---
+++ Query_33.sql
@@ -0,0 +1,23 @@
+with given(project_id) as
+(select project_id 
+from scientific_field_project
+where scfield_id = 36649
+)
+select title 
+from project 
+where project.project_id in
+(select project_id from given);
+
+with given(project_id) as
+(select project_id 
+from scientific_field_project
+where scfield_id = 36649
+),
+resIDs(researcher_id) as
+(select researcher_id 
+from works_on_project 
+where works_on_project.project_id in
+(select project_id from given))
+select first_name, last_name from researcher 
+where researcher.researcher_id in 
+(select researcher_id from resIDs);
