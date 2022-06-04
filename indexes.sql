8c2f261e99e46e1564eab00a201802dd760d8af4
Pluetsy <105885179+Pluetsy@users.noreply.github.com>
2022-06-04 16:08:12 +0300

    Delete indexes.sql

--- indexes.sql
+++
@@ -1,6 +0,0 @@
--- Create Indexes
-
-CREATE INDEX idx_res_fname ON researcher (first_name);
-CREATE INDEX idx_res_lname ON researcher (last_name);
-CREATE INDEX idx_p_s_date ON project (start_date);
-CREATE INDEX idx_p_e_date ON project (end_date);
