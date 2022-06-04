cf546804442c1af60dcfc8fbdab40952fa773ecb
Pluetsy <105885179+Pluetsy@users.noreply.github.com>
2022-06-04 16:09:36 +0300

    Delete 3.7Query.sql

--- 3.7Query.sql
+++
@@ -1,7 +0,0 @@
-
-
-with budget_pr(executive_id, organization_id, value)as
-(select executive_id, organization_id, -1*sum(funds) from project group by executive_id, organization_id) ,
-top5ing(executive_id, organization_id, value) as
-(select executive_id, organization_id, -1*value from budget_pr group by value limit 5 )
-select name_ex, name_org, value from executive, organization, top5ing where (executive.executive_id, organization.organization_id) = (top5ing.executive_id,top5ing.organization_id);
